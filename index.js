43ad5a090b02205bb7a8c9616c6b0894af6c723f
GLobalYZ <globalyy2020@gmail.com>
2023-05-08 16:45:17 -0700

    finish project general setup

---
+++ index.js
@@ -0,0 +1,113 @@
+require("./utils.js");
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const mongoDBSession = require('connect-mongodb-session')(session);
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const Joi = require("joi");
+
+
+const expireTime =  60 * 60 * 1000; 
+
+//control the strength of the password
+const saltRounds = 6;
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+//declare we use ejs res.render will use ejs the ejs files created under views
+app.set('view engine', 'ejs');
+
+//false: url decode only support array or string
+app.use(express.urlencoded({extended: false}))
+
+const store = new mongoDBSession({
+    uri: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority`,
+    collection:"mySessions",
+    crypto: {
+		secret: mongodb_session_secret
+	}
+});
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: store, //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+// middleware function starts
+function isValidSession(req) {
+    if (req.session.authenticated) {
+        return true;
+    }
+    return false;
+}
+
+function sessionValidation(req,res,next) {
+    if (isValidSession(req)) {
+        next();
+    }
+    else {
+        res.redirect('/login');
+    }
+}
+
+
+function isAdmin(req) {
+    if (req.session.type == 'admin') {
+        return true;
+    }
+    return false;
+}
+
+function adminAuthorization(req, res, next) {
+    if (!isAdmin(req)) {
+        res.status(403);
+        res.render("errorMessage", {error: "Not Authorized"});
+        return;
+    }
+    else {
+        next();
+    }
+}
+// middleware function finishes
+
+app.get('/', (req,res) => {
+    // check connection
+    // console.log(userCollection)
+    res.render("index");
+});
+
+
+//static images address
+app.use(express.static(__dirname + "/public"));
+
+// handle 404 - page not found
+// must put this in the very last otherwise it will catch all routes as 404
+app.get("*", (req,res) => {
+	res.status(404);
+	res.render("404");
+})
+
+
+app.listen(port, ()=>{
+    console.log("Node application listening on part" + port)
+})
