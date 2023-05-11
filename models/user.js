4daae91c06cf240fb28619ebde0f6cf541070573
I-wen Su <grace2268@hotmail.com>
2023-05-11 15:09:53 -0700

    add local database connection lines (commented out)

--- models/user.js
+++ models/user.js
@@ -5,6 +5,18 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
 
+// mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1', {  //connect to the local database
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(
+//     () => {
+//         console.log("Connected to MongoDB successfully!");
+//     },
+//     err => {
+//         console.log("Connection to MongoDB failed!" + err);
+//     }
+// );
+
 mongoose.connect(`mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
