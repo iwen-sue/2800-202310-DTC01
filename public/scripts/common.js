7c9712434802e020f76e2bab734bc306d49e50f3
I-wen Su <grace2268@hotmail.com>
2023-05-10 18:06:37 -0700

    nav script

---
+++ public/scripts/common.js
@@ -0,0 +1,23 @@
+// this file includes common functions we need for all pages
+
+// for headers and navs
+/* Set the width of the side navigation to 250px */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function setup() {
+    console.log("setup!");
+    $(".content").click(function () {
+        console.log("div clicked!");
+        closeNav();
+    });
+}
+
+$(document).ready(setup);
