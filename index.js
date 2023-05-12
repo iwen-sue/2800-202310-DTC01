bf2bd37fadbad03ea87d58f444a97ebc85eca2e5
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-05-12 01:08:01 -0700

    added creating a group and invite member function

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const mongoDBSession = require('connect-mongodb-session')(session);
 // const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const usersModel = require('./models/user.js');
+const groupsModel = require('./models/group.js');
 const ejs = require('ejs');
+
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+    }
+});
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -32,6 +42,7 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 
 var { database } = include('databaseConnection');
 
+const groupCollection = database.db(mongodb_database).collection('groups');
 const userCollection = database.db(mongodb_database).collection('users');
 
 //declare we use ejs res.render will use ejs the ejs files created under views
@@ -94,10 +105,15 @@ function adminAuthorization(req, res, next) {
 // middleware function finishes
 
 app.get('/', (req, res) => {
-    res.render("index");
+    if (isValidSession(req)) {
+        res.redirect('/home');
+    }
+    else {
+        res.render("index");
+    }
 });
 
-app.get('/home', (req, res) => {
+app.get('/home', sessionValidation,(req, res) => {
     res.render("home");
 });
 
@@ -106,14 +122,19 @@ app.get('/userprofile', (req,res) => {
 });
 
 app.get('/signup', (req, res) => {
-    res.render("signup");
+    console.log(req.query.groupToken)
+    if (req.query.groupToken != null) {
+        console.log("groupToken: " + req.query.groupToken);
+        res.render("signup", { groupToken: req.query.groupToken });
+    }
+    else {
+        res.render("signup", { groupToken: null });
+    }
 });
 
 app.get('/login', (req, res) => {
     res.render("login");
 });
-
-//Test Post
 
 app.post('/signup', async (req, res) => {
     try {
@@ -122,13 +143,20 @@ app.post('/signup', async (req, res) => {
             // Render the signup page with an error message
             return res.render('signup', { error: 'MissingFields' });
         }
-
+        console.log(req.body.groupToken, 'groupToken')
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        var userType
+        if (req.body.groupToken != null) {
+            userType = 'member'
+            await groupsModel.updateOne({ _id: req.body.groupToken }, { $push: { members: req.body.email } }).exec();
+        }
         const user = new usersModel({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             email: req.body.email,
-            password: hashedPassword
+            password: hashedPassword,
+            groupID: req.body.groupToken,
+            type: userType
         });
         await user.save();
         res.redirect('/login');
@@ -137,32 +165,6 @@ app.post('/signup', async (req, res) => {
         res.redirect('/signup?error=ServerError');
     }
 });
-
-
-
-
-// app.post('/signup', async (req, res) => {
-
-//     try {
-//         console.log("hit signup post")
-//         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//         console.log("hashedPassword: " + hashedPassword);
-//         console.log("req.body.email: " + req.body.email);
-//         const user = new usersModel({
-//             firstName: req.body.firstName,
-//             lastName: req.body.lastName,
-//             email: req.body.email,
-//             password: hashedPassword
-//         });
-//         await user.save();
-//         res.redirect('/login');
-//     } catch {
-//         res.redirect('/signup');
-//     }
-// }
-// );
-
-
 
 app.post('/login', async (req, res) => {
     var password = req.body.password;
@@ -197,58 +199,73 @@ app.post('/login', async (req, res) => {
     }
 });
 
+app.get('/creategroup', sessionValidation,(req, res) => {
+    res.render("creategroup", { error: null });
+});
 
-
-// app.post('/login', async (req, res) => {
-//     var password = req.body.password;
-//     var email = req.body.email;
-
-//     const schema = Joi.string().max(20).required();
-//     const validationResult = schema.validate(email);
-//     if (validationResult.error != null) {
-//         console.log("email error :", validationResult.error);
-//         res.redirect("/login");
-//         return;
-//     }
-    
-//     const result = await usersModel.find({ email: email }).select('email type firstName lastName password _id').exec();
-//     console.log("result: ", result);
-//     console.log("password: ", result[0].password);
-//     console.log("email: ", result[0].email);
-//     if (result.length == 0) {
-//         var message = "User is not found";
-//         res.render("loginError", { error: message });
-//     }
-//     if (await bcrypt.compare(password, result[0].password)) {
-//         req.session.authenticated = true;
-//         req.session.lastName = result[0].lastName;
-//         req.session.firstName = result[0].firstName;
-//         req.session.password = result[0].password;
-//         req.session.email = result[0].email;
-//         req.session.cookie.maxAge = expireTime;
-
-//         res.redirect('/home');
-//     }
-//     else {
-//         var message = "Password is not correct";
-//         res.render("loginError", { error: message });
-//     }
-// });
-
-
-
-
-    //static images address
-    app.use(express.static(__dirname + "/public"));
-
-    // handle 404 - page not found
-    // must put this in the very last otherwise it will catch all routes as 404
-    app.get("*", (req, res) => {
-        res.status(404);
-        res.render("404");
-    })
-
-
-    app.listen(port, () => {
-        console.log("Node application listening on port " + port);
+app.post('/groupconfirm', sessionValidation, async (req, res) => {
+    var groupName = req.body.groupName;
+    const schema = Joi.string().max(20).required();
+    const validationResult = schema.validate(groupName);
+    if (validationResult.error != null) {
+        var error = "Group name is too long. Please enter a group name that is 20 characters or less.";
+        return res.render("creategroup", { error: error });
+    }
+    const newGroup = new groupsModel({
+        groupName: groupName,
+        members: [req.session.email]
     });
+    await newGroup.save();
+    const group = await groupsModel.findOne({ groupName: groupName }).exec();
+    await usersModel.updateOne({ email: req.session.email }, { $set: { groupID: group._id, type: 'leader' } }).exec();
+    res.render("groupconfirm", { groupName: req.body.groupName});
+});
+
+app.get('/userprofile/groupdetails', sessionValidation, async (req, res) => {
+    var currentUser = await usersModel.findOne({ email: req.session.email }).exec()
+    const group = await groupsModel.findOne({ _id: currentUser.groupID }).exec()
+    var allMembers = group.members
+
+    const memberNames = allMembers.map(async (member) => {
+        member = await usersModel.findOne({ email: member }).exec()
+        return member.firstName + " " + member.lastName;
+    })
+    Promise.all(memberNames).then((memberNames) => {
+        res.render("groupdetails", { user: currentUser, group: memberNames, groupName: group.groupName, groupID: group._id, confirmation: null });
+    }, (err) => {
+        console.log(err)
+    })
+});
+
+app.post('/invitesent', sessionValidation, async (req, res) => {
+    var inviteEmail = req.body.inviteeEmail;
+    const userName = req.session.firstName + " " + req.session.lastName;
+    const groupToken = req.body.groupID
+    const inviteMessage = {
+        from: process.env.EMAIL,
+        to: inviteEmail,
+        subject: 'You have been invited to join a group on VacaPal!',
+        html: `<h1>You have been invited by ${userName} to join their group on VacaPal!</h1>
+        <p>Click <a href="http://localhost:3000/signup?groupToken=${groupToken}">here</a> to sign up and join the group!</p>`
+}
+
+    transporter.sendMail(inviteMessage, (err, info) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(info.response);
+        }
+    });
+    res.redirect('/userprofile/groupdetails');
+});
+//static images address
+app.use(express.static(__dirname + "/public"));
+// handle 404 - page not found
+// must put this in the very last otherwise it will catch all routes as 404
+app.get("*", (req, res) => {
+    res.status(404);
+    res.render("404");
+})
+app.listen(port, () => {
+    console.log("Node application listening on port " + port);
+});
