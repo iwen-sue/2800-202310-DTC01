c68195c91c6f966c636ec010178510c4cf2b2cbe
I-wen Su <grace2268@hotmail.com>
2023-05-13 22:41:01 -0700

    image upload under each card in buckelist

--- editBucket.js
+++ editBucket.js
@@ -4,10 +4,33 @@ const express = require('express');
 const usersModel = require('./models/user.js');
 const router = express.Router();
 
-router.post('/editBucket', async (req, res) => {
-    const result = await usersModel.findOne({
+const multer = require('multer');  // npm install multer
+const storage = multer.memoryStorage(); // store the file in memory as a buffer
+const upload = multer({ storage: storage }); // specify the storage option
+
+
+
+router.post('/editBucket',upload.single('avatar'), async (req, res) => {
+    const query = usersModel.findOne({
         email: req.session.email,
     });
+    const cardID = req.body.cardID
+    const condition = { "bucketlist._id": cardID };
+    const update = {
+        $set: {
+            "bucketlist.$.description": req.body.description,
+            "bucketlist.$.travelImg": req.file? req.file.buffer.toString('base64'): undefined,
+        }
+    };
+    const options = { upsert: true };
+    const result = await query.exec();
+    result.bucketlist.forEach(async (item) => {
+        if (item._id == cardID) {
+            await usersModel.updateOne(condition, update, options);
+        }
+    }
+    );
+    res.redirect('/userprofile');
 
 });
 
--- index.js
+++ index.js
@@ -148,15 +148,20 @@ app.get('/editBucket', (req, res) => {
     const query = usersModel.findOne({
         email: req.session.email,
     });
-    query.then((docs) => {
-        res.render("editBucket", { user: docs });
-
-    }).catch((err) => {
+    const cardID = req.query.cardID;
+    query.then((docs) => {docs.bucketlist.forEach((card) => {
+        if (card._id == cardID) {
+            res.render("editBucket", { card: card });
+        }
+    }
+    )}).catch((err) => {
         console.error(err);
-    });
-});
-app.get('/enterBucket', (req, res) => {
+    }
+    );
     
+});
+
+app.get('/enterBucket', (req, res) => {
     res.render("enterBucket");
 });
 app.get('/userprofile/travel_history', (req, res) => {
